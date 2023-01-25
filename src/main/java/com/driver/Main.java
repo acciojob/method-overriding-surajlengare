8a021ddebdd0256d32ad2cb458d817bb0ea664de
surajlengare <suraj.lengare@rsllearning.com>
2023-01-25 19:52:02 +0530

    Answer 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,5 +9,24 @@ public class Main
         B obj1 = new B();
         System.out.println(obj1.meth());
     }
-
 }
+
+class A
+{
+    public String meth()
+    {
+        return ("Invoking method from class A");
+    }
+}
+
+class B extends A
+{
+    @Override
+    public String meth()
+    {
+        return ("Method is overridden in Extendend class B");
+    }
+}
+
+
+
