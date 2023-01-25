5ba9401d95b6ed125bdae20787b8601ea259804d
surajlengare <suraj.lengare@rsllearning.com>
2023-01-25 19:36:09 +0530

    ans 1

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+public class B extends A
+{   @Override
+    public String meth()
+        {
+          return ("Method is overridden in Extendend class B");
+        }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
-public class Main {
-  
+import org.w3c.dom.ls.LSOutput;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        B obj1 = new B();
+        System.out.println(obj1.meth());
+    }
+
 }
